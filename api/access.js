3f6c1a9c4870dce5130535d84e93dea2193797fe
Luxalry <luxalry.benoua@gmail.com>
2026-08-17 04:08:27 +0100

    Update access.js

--- api/access.js
+++ api/access.js
@@ -1,5 +1,5 @@
-
 import crypto from 'crypto';
+import { handleAdminCors } from './utils.js';
 
 // Configuration
 const TG_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
@@ -69,14 +69,8 @@ async function answerCallbackQuery(callbackQueryId, text) {
 
 export default async function handler(req, res) {
     // CORS
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
+    if (handleAdminCors(req, res)) {
+        return res.status(200).end();
     }
 
     const { action } = req.query;
